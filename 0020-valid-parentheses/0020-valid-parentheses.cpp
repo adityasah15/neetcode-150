04177ef359dc072e6bf1c538fab2f8ff814bdcec
adityasah15 <163900214+adityasah15@users.noreply.github.com>
2026-07-16 13:56:57 +0530

    Time: 0 ms (100%), Space: 10.1 MB (6.89%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        if (s.size() % 2 != 0)
+            return false;
+        stack<char> st;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(')
+                st.push(')');
+            else if (s[i] == '{')
+                st.push('}');
+            else if (s[i] == '[')
+                st.push(']');
+            else {
+                if (st.empty() || st.top() != s[i]) {
+                    return false;
+                }
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
