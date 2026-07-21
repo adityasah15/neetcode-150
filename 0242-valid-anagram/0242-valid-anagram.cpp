691ebe87327c5732fea3534cf07f7adf5c77c09e
adityasah15 <163900214+adityasah15@users.noreply.github.com>
2026-07-21 19:44:02 +0530

    Time: 2 ms (50.68%), Space: 9.7 MB (76.41%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -3,14 +3,13 @@ public:
     bool isAnagram(string s, string t) {
         if (s.size() != t.size())
             return false;
-        int freq1[26] = {0};
-        int freq2[26] = {0};
+        int freq[26] = {0};
         for (int i = 0; i < s.size(); i++) {
-            freq1[s[i] - 'a']++;
-            freq2[t[i] - 'a']++;
+            freq[s[i] - 'a']++;
+            freq[t[i] - 'a']--;
         }
         for (int i = 0; i < 26; i++) {
-            if (freq1[i] != freq2[i]) {
+            if (freq[i] != 0) {
                 return false;
             }
         }
